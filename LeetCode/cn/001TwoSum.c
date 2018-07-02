88e3aa846e4d36eca6040c8d13f181da5fec419a
dyw <2909638069@qq.com>
2018-07-02 15:42:02 +0800

    Update 001TwoSum.c

--- LeetCode/cn/001TwoSum.c
+++ LeetCode/cn/001TwoSum.c
@@ -4,13 +4,21 @@
 int* twoSum(int* nums, int numsSize, int target) {
     int i, min = nums[0];
     int* indices = malloc(2*sizeof(int));
+    /* find the min of nums[numsSize] */
     for (i = 1; i < numsSize; i++)
         if (min > nums[i])
             min = nums[i];
+    
     int hashSize = target - min - min + 1;
     int hash[hashSize];
+    /* initialization of hash */
     for (i = 0; i < hashSize; i++)
         hash[i] = INT_MIN;
+    /* loop of nums
+       1 skip the elements larger than target-min
+       2 tag the corresponding element
+       3 when find the tag, return the two indices
+    */
     for (i = 0; i < numsSize; i++){
         if (nums[i] > target-min)
             continue;
